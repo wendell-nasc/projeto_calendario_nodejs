e1b4825cfc161b44aba76e2cf45f1ff962f8118b
Wendell Silva <wendellxj@hotmail.com>
2024-09-11 11:31:53 -0300

    versão 2 - workbox

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 
 workbox.loadModule('workbox-background-sync');
 
-workbox.precaching.precacheAndRoute( [{"revision":"da84913bf34405cdcf82272e9a6026e1","url":"assets/index.9ce47bf7.css"},{"revision":"985f2114b3d468496c7adbe1c5579ff3","url":"assets/index.fc19b8f1.js"},{"revision":"e888e62d682b20f13cbfeb4b75447109","url":"index.html"}] );
+workbox.precaching.precacheAndRoute( [{"revision":"da84913bf34405cdcf82272e9a6026e1","url":"assets/index.9ce47bf7.css"},{"revision":"985f2114b3d468496c7adbe1c5579ff3","url":"assets/index.fc19b8f1.js"},{"revision":"edd52901062496d730f798529c367629","url":"index.html"}] );
 
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
@@ -13,7 +13,7 @@ const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 const cacheNetworkFirst = [
     '/api/auth/renew',
-    '/api/events',
+    '/api/events',    
 ]
 
 registerRoute(
@@ -75,6 +75,41 @@ registerRoute(
 
 registerRoute(
     new RegExp('http://localhost:4000/api/events/'),
+    new NetworkOnly({
+        plugins: [ bgSyncPlugin ]
+    }),
+    'PUT'
+)
+
+
+
+
+
+
+
+//Producao
+
+// https://wendell-calendario-cristino.up.railway.app/api/auth
+
+
+registerRoute(
+    new RegExp('https://wendell-calendario-cristino.up.railway.app/api/events'),
+    new NetworkOnly({
+        plugins: [ bgSyncPlugin ]
+    }),
+    'POST'
+)
+
+registerRoute(
+    new RegExp('https://wendell-calendario-cristino.up.railway.app/api/events/'),
+    new NetworkOnly({
+        plugins: [ bgSyncPlugin ]
+    }),
+    'DELETE'
+)
+
+registerRoute(
+    new RegExp('https://wendell-calendario-cristino.up.railway.app/api/events/'),
     new NetworkOnly({
         plugins: [ bgSyncPlugin ]
     }),
